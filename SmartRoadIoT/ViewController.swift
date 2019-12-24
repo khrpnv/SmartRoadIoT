99e8bf15284005a56fa1a2fd668b89fc74d7dfc3
Illia Khrypunov <illia.khrypunov@nure.ua>
2019-12-24 10:50:16 +0200

    Initial Commit

---
+++ SmartRoadIoT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartRoadIoT
+//
+//  Created by Illia Khrypunov on 12/24/19.
+//  Copyright © 2019 Illia Khrypunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
